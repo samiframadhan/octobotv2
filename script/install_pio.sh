b3254aab9277e4a605685b723f8af8405ec9e8b4
samifr <sfauzanramadhan@gmail.com>
2024-10-29 09:48:06 +0700

    [Add] Installation script

---
+++ script/install_pio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 get_pio.py
+
+ln -s ~/.platformio/penv/bin/platformio ~/.local/bin/platformio
+ln -s ~/.platformio/penv/bin/pio ~/.local/bin/pio
+ln -s ~/.platformio/penv/bin/piodebuggdb ~/.local/bin/piodebuggdb
